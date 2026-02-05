5fa9304102f85c09236c1cc1e87fae9d116f1eb9
sararyslanbarvna-stack <sararyslanbarvna@gmail.com>
2026-02-05 09:33:47 +0500

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -7,14 +7,14 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-       
-        colorScheme: .fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: Colors.deepPurple,
+        ),
       ),
       home: const MyHomePage(title: 'I LOVE BOSTON'),
     );
@@ -24,8 +24,6 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
 
- 
-
   final String title;
 
   @override
@@ -33,35 +31,50 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  
-
   @override
   Widget build(BuildContext context) {
-   
     return Scaffold(
       appBar: AppBar(
-       
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-       
         title: Text(widget.title),
       ),
       body: Center(
-        
-       child: Column(
-  mainAxisAlignment: MainAxisAlignment.center,
-  children: [
-    Image.asset(
-      "assets/images/Снимок экрана 2026-02-03 в 09.13.38.png",
-    
-    ),
-     Image.asset(
-      "assets/images/Снимок экрана 2026-02-03 в 09.13.56.png",
-    
-    ),
-  ],
-),
-
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset(
+              "assets/images/Снимок экрана 2026-02-03 в 09.13.38.png",
+            ),
+            Image.asset(
+              "assets/images/Снимок экрана 2026-02-03 в 09.13.56.png",
+            ),
+            const SizedBox(height: 30),
+            buildTypesButton(),
+          ],
         ),
-      );
+      ),
+    );
   }
+}
+
+Widget buildTypesButton() {
+  return Column(
+    mainAxisAlignment: MainAxisAlignment.center,
+    children: [
+      TextButton(
+        onPressed: () {},
+        child: const Text('TextButton'),
+      ),
+      const SizedBox(height: 20),
+      ElevatedButton(
+        onPressed: () {},
+        child: const Text('ElevatedButton'),
+      ),
+      const SizedBox(height: 20),
+      OutlinedButton(
+        onPressed: () {},
+        child: const Text('OutlinedButton'),
+      ),
+    ],
+  );
 }
